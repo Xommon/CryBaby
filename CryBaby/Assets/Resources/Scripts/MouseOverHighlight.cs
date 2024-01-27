fbb541e4b80275a0303b2e26dd090d19ec34f034
Jason <jason.r.bennett82@gmail.com>
2024-01-27 11:16:04 -0600

    Simple Object Highlighting

---
+++ CryBaby/Assets/Resources/Scripts/MouseOverHighlight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverHighlight : MonoBehaviour
+{
+    [SerializeField]
+    private Material highlightMaterial;
+    private Material defaultMaterial;
+    [SerializeField]
+    private SpriteRenderer[] highlightedSprites;
+
+    void OnMouseEnter()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+        defaultMaterial = highlightedSprites[0].material;
+        for (int i = 0; i < highlightedSprites.Length; i++)
+        {
+            Debug.Log(i);
+            highlightedSprites[i].material = highlightMaterial;
+        }
+        
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+        for (int i = 0; i < highlightedSprites.Length; i++)
+        {
+            highlightedSprites[i].material = defaultMaterial;
+        }
+    }
+}
